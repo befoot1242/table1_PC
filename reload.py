3a69bc8508c1132723d463e98441485297073c39
yo-agawa <a40g89ks@gmail.com>
2024-08-01 00:18:47 +0900

    aaaa

--- reload.py
+++ reload.py
@@ -5,19 +5,26 @@ import json
 with open('results.json', 'r', encoding='utf-8') as file:
     results = json.load(file)
 
-out = "<!DOCTYPE html>\n"
-out += "<html lang='ja'>\n"
-out += "<table>\n"
-out += "<tbody>\n"
-out += "<tr><td>?<td><td>バサラ大将<td><td>坂本<td><td>尊敬ラーメン屋<td><td>ゆっけ<td></tr>\n"
-for ar in results:
-    row = "<tr>"
-    for td in ar:
-        row += "<td>" + str(td) + "</td>"
-    row += "</tr>\n"
-    out += row
-out += "</tbody>"
-out += "</table>\n"
-out += "</html>\n"
+# out = "<!DOCTYPE html>\n"
+# out += "<html lang='ja'>\n"
+# out += "<table>\n"
+# out += "<tbody>\n"
+# out += "<tr><td>?</td><td>バサラ大将</td><td>坂本</td><td>尊敬ラーメン屋</td><td>ゆっけ</td></tr>\n"
+# for ar in results:
+#     row = "<tr>"
+#     for td in ar:
+#         row += "<td>" + str(td) + "</td>"
+#     row += "</tr>\n"
+#     out += row
+# out += "</tbody>"
+# out += "</table>\n"
+# out += "</html>\n"
+
+out = '$(document).ready(function() {\n' + '// JSONデータ\n' + 'var jsonData = [\n'
+out += str(results[0])
+for i in range(1,len(results)):
+    out += "," + str(results[i])
+out += "        // 他のデータ...\n"+"    ];\n"+"    // DataTableの初期化\n"+"    $('#example').DataTable({\n"+"        data: jsonData,\n"+"        columns: [\n"+"            { title: \"Column A\" },\n"+"            { title: \"Column B\" },\n"+"            { title: \"Column C\" },\n"+"            { title: \"Column D\" },\n"+"            { title: \"Column E\" }\n"+"        ]\n"+"    });\n"+"});\n"
+
 
 print(out)
