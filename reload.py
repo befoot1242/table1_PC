415528b841dd4b2a910c9a1cce65edc6ad6c12e5
yo-agawa <a40g89ks@gmail.com>
2024-07-31 23:37:22 +0900

    "Commit Log"

--- reload.py
+++ reload.py
@@ -5,12 +5,19 @@ import json
 with open('results.json', 'r', encoding='utf-8') as file:
     results = json.load(file)
 
-out = "<tbody>"
+out = "<!DOCTYPE html>\n"
+out += "<html lang='ja'>\n"
+out += "<table>\n"
+out += "<tbody>\n"
+out += "<tr><td>?<td><td>バサラ大将<td><td>坂本<td><td>尊敬ラーメン屋<td><td>ゆっけ<td></tr>\n"
 for ar in results:
     row = "<tr>"
     for td in ar:
         row += "<td>" + str(td) + "</td>"
-    row = "</tr>\n"
+    row += "</tr>\n"
+    out += row
 out += "</tbody>"
+out += "</table>\n"
+out += "</html>\n"
 
 print(out)
