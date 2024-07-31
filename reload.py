31d488d2c9659e032683b45109de1e2ed18e8ee1
yo-agawa <a40g89ks@gmail.com>
2024-08-01 00:45:03 +0900

    aaaa

--- reload.py
+++ reload.py
@@ -1,30 +1,28 @@
-import numpy as np
-import codecs
 import json
 
 with open('results.json', 'r', encoding='utf-8') as file:
     results = json.load(file)
 
-# out = "<!DOCTYPE html>\n"
-# out += "<html lang='ja'>\n"
-# out += "<table>\n"
-# out += "<tbody>\n"
-# out += "<tr><td>?</td><td>バサラ大将</td><td>坂本</td><td>尊敬ラーメン屋</td><td>ゆっけ</td></tr>\n"
-# for ar in results:
-#     row = "<tr>"
-#     for td in ar:
-#         row += "<td>" + str(td) + "</td>"
-#     row += "</tr>\n"
-#     out += row
-# out += "</tbody>"
-# out += "</table>\n"
-# out += "</html>\n"
-
 out = '$(document).ready(function() {\n' + '// JSONデータ\n' + 'var jsonData = [\n'
 out += str(results[0])
 for i in range(1,len(results)):
-    out += "," + str(results[i])
-out += "        // 他のデータ...\n"+"    ];\n"+"    // DataTableの初期化\n"+"    $('#example').DataTable({\n"+"        data: jsonData,\n"+"        columns: [\n"+"            { title: \"Column\" },\n"+"            { title: \"バサラ大将\" },\n"+"            { title: \"坂本\" },\n"+"            { title: \"尊敬ラーメン屋\" },\n"+"            { title: \"ゆっけ\" },\n"+"            { title: \"voter\" }\n"+"        ]\n"+"    });\n"+"});\n"
+    out += "," + str(results[i]) + '\n'
+out += "        // 他のデータ...\n"
+out += "    ];\n"
+out += "    // DataTableの初期化\n"
+out += "debugger;"
+out += "    $('#example').DataTable({\n"
+out += "        data: jsonData,\n"
+out += "        columns: [\n"
+out += "            { title: \"Column\" },\n"
+out += "            { title: \"バサラ大将\" },\n"
+out += "            { title: \"坂本\" },\n"
+out += "            { title: \"尊敬ラーメン屋\" },\n"
+out += "            { title: \"ゆっけ\" },\n"
+out += "            { title: \"voter\" }\n"
+out += "        ]\n"
+out += "    });\n"
+out += "});\n"
 
 
 print(out)
